d61830ffbd98370d8aff8de30e82c95cb9a2dd83
gw2102961 <kimminha@gabia.com>
2021-06-22 08:30:43 +0900

    0622: 일부 컴포넌트 생성 및 스타일링

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    header
-  </div>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
 </template>
 
 <script lang='ts'>
@@ -26,5 +26,10 @@ export default Vue.extend({
 });
 </script>
 
-<style lang='scss'>
+<style scoped>
+h1 {
+  color: #2f3b52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
 </style>
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,29 +1,30 @@
 <template>
   <div>
-    list
+    <ul>
+      <li v-for="todoItem in todoItems" :key="todoItem">{{ todoItem }}</li>
+    </ul>
   </div>
 </template>
 
-<script lang='ts'>
-import Vue from 'vue';
-export default Vue.extend({
-  components: {},
-  props: {},
+<script>
+export default {
   data() {
-    return {};
+    return {
+      todoItems: []
+    };
+  // 인스턴스가 생성되자마자 호출되는 라이프사이클 훅
   },
-  computed: {},
-  watch: {},
-  beforeCreate() {},
-  created() {},
-  beforeMount() {},
-  mounted() {},
-  beforeUpdate() {},
-  updated() {},
-  beforeDestroy() {},
-  destroyed() {},
-  methods: {},
-});
+  created() {
+    if(localStorage.length > 0) {
+      for(var i = 0; i < localStorage.length; i ++){
+        if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+          this.todoItems.push(localStorage.key(i));
+        }
+      }
+    }
+  },
+};
 </script>
 
-<style lang='scss' scoped></style>
+<style scoped>
+</style>
